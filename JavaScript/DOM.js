57909486887a8df4eeaff81d2ffea1bc52974c46
lstheart <1042138926@qq.com>
2021-02-04 23:09:39 +0800

    ✨feat(day16):
    10.11.12章学习:
    DOM操作技术以及DOM扩展,DOM2和DOM3
    
    明日: JS 13,14

---
+++ JavaScript/DOM.js
@@ -0,0 +1,26 @@
+var originTitle=document.title;
+document.title="DOM学习";
+
+var myDiv2=document.getElementsByTagName("input");
+
+document.write("<strong>" + (new Date()).toString() + "</strong>");
+document.write("<br/>are you ok?");
+
+
+// var div3 = document.createElement("div");
+
+// div3.setUserData("name", "Nicholas", function(operation, key, value, src, dest){
+//     if (operation == 1){
+//         dest.setUserData(key, value, function(){});   }
+//     });
+
+// var newDiv = div.cloneNode(true);
+// alert(newDiv.getUserData("name"));
+
+var myDiv = document.getElementById("myDiv");
+myDiv.style.backgroundColor = "red";
+myDiv.style.border = "solid";
+// alert(myDiv.style.cssText);
+for (var i=0, len=myDiv.style.length; i < len; i++){
+    alert(myDiv.style[i]); //或者 myDiv.style.item(i)
+}
