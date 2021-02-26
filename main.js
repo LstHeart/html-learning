957ef8f86003129b3177065edfd619eb266bb0d8
lstheart <1042138926@qq.com>
2021-02-26 22:13:50 +0800

    lean vue

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2021, lstheart. All rights reserved.
+ *
+ * @author lstheart
+ * @date 2021-02-17
+ * @version 0.0.1
+ */
+import Vue from 'vue';
+import * as BOM from './JavaScript\BOM'
+
+const vm = new Vue({
+
+});
